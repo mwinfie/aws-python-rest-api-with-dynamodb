3f348aa250bb8a4557ad3b564e8f01681e2758ab
Matthew Winfield <mwinfie@gmail.com>
2018-11-09 18:34:23 -0600

    Adding modified example files from serverless\examples repository.

---
+++ data/create.py
@@ -0,0 +1,38 @@
+import json
+import logging
+import os
+import time
+import uuid
+
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+
+def create(event, context):
+    data = json.loads(event['body'])
+    if 'temperature' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't create the data item.")
+        return
+
+    timestamp = int(time.time() * 1000)
+
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    item = {
+        'id': str(uuid.uuid1()),
+        'temperature': data['temperature'],
+        'checked': False,
+        'timestamp': timestamp,
+    }
+
+    # write the data to the database
+    table.put_item(Item=item)
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(item)
+    }
+
+    return response
